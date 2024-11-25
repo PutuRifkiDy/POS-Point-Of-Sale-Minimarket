3e0e36692585866f9d6b1d6d050878c8f351ec90
PutuRifkiDy <puturifki56@gmail.com>
2024-11-25 11:33:33 +0800

    add tailwind and login

--- pos-minimarket/resources/views/layouts/sidebar.blade.php
+++ pos-minimarket/resources/views/layouts/sidebar.blade.php
@@ -14,9 +14,8 @@
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu" data-widget="tree">
             <li>
-                <a href="#">
+                <a href="{{route('dashboard')}}">
                     <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-
                 </a>
             </li>
             <li class="header">MASTER</li>
